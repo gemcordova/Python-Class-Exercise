8768be088847d4bde43f86ec3ab33c25cc9df2f9
Gem Cordova <gemcordova2006@gmail.com>
2025-11-22 22:08:02 +0800

    trouble shoot

--- Exercises/exercise-01.py
+++
@@ -1,6 +0,0 @@
-# Exercise 1: Multiplication Table Generator
-
-num01 = int(input('Enter a number: '))
-
-for product in range(1, 11):
-    print(f'{num01} x {product} = {num01 * product}')
--- Exercises/exercise-03.py
+++
@@ -1,6 +0,0 @@
-# Exercise 3: Star Pattern Pyramid
-
-row = int(input('Enter number of row: '))
-
-for star in range(1, row + 1):
-    print('*' * star)
